ab9cb4173c0aa3ae9e10cc3fec46b9800748f75f
Louis Tan <louistan600@gmail.com>
2026-04-17 18:34:03 +0800

    add message model

---
+++ api/Models/Message.cs
@@ -0,0 +1,14 @@
+namespace api.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public bool IsRead { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public int? ParentMessageId { get; set; }
+        public bool IsUnsent { get; set; }
+    }
+}
