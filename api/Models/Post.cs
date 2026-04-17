d6197ec9cc1dfb2544e5a513096d139b5299f15a
Louis Tan <louistan600@gmail.com>
2026-04-17 18:33:47 +0800

    add post model

---
+++ api/Models/Post.cs
@@ -0,0 +1,12 @@
+namespace api.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
